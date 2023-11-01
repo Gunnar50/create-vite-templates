67b7b7653604b5671250d5d08f4a27ca60bb8467
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-11-01 08:51:27 +0000

    added new template for testing

--- cli.js
+++ cli.js
@@ -2,16 +2,25 @@
 
 const fs = require("fs-extra");
 const path = require("path");
+const readline = require("readline");
 
-// Get the name of the new project folder from the command line arguments
-const projectFolder = process.argv[2] || "my-new-project";
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
 
-// Paths for the template folder and the new project folder
-const templatePath = path.join(__dirname, "template");
-const projectPath = path.join(process.cwd(), projectFolder);
+// Templates available
+const templates = {
+	react: "template-reactjs",
+	"react+redux": "template-reactjs_redux",
+};
 
-// Function to copy the template into a new project directory
-async function createProject() {
+// Function to create the project
+async function createProject(template) {
+	const templatePath = path.join(__dirname, templates[template]);
+	const projectName = "my-new-project"; // You can make this dynamic
+	const projectPath = path.join(process.cwd(), projectName);
+
 	try {
 		await fs.copy(templatePath, projectPath);
 		console.log(`Project created at ${projectPath}`);
@@ -20,5 +29,31 @@ async function createProject() {
 	}
 }
 
-// Run the function
-createProject();
+// If a template name is provided directly
+if (process.argv[2]) {
+	const template = process.argv[2];
+	if (!templates[template]) {
+		console.error("Template not found");
+		process.exit(1);
+	}
+	createProject(template);
+} else {
+	// Otherwise, show the user the available templates
+	console.log("Please choose a template:");
+	const templateNames = Object.keys(templates);
+	templateNames.forEach((name, index) => {
+		console.log(`${index + 1}. ${name}`);
+	});
+
+	rl.question("Your choice: ", (answer) => {
+		const index = parseInt(answer) - 1;
+		if (isNaN(index) || index < 0 || index >= templateNames.length) {
+			console.error("Invalid choice");
+			rl.close();
+			process.exit(1);
+		}
+
+		createProject(templateNames[index]);
+		rl.close();
+	});
+}
